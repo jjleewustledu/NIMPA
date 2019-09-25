0e09437ad4221d1ab406fd78e9812c0f2d77cda2
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-09-24 19:10:30 -0500

    Updates from Caspar

---
+++ build/nimpa/dinf/CMakeFiles/dinf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dinf.dir/dinf_generated_devprop.cu.o"
+  "CMakeFiles/dinf.dir/dinf_generated_dinf_module.cu.o"
+  "dinf.pdb"
+  "dinf.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/dinf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/nimpa/prc/CMakeFiles/improc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/improc.dir/src/improc_generated_conv.cu.o"
+  "CMakeFiles/improc.dir/src/improc_generated_img_module.cu.o"
+  "CMakeFiles/improc.dir/src/improc_generated_rsmpl.cu.o"
+  "improc.pdb"
+  "improc.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/improc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
